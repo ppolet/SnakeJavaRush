0c97dee411c54dacaab0497d8655dd950db848d0
Pavel <USER@DESKTOP-IQNR3UP>
2020-06-20 00:52:03 +0300

    Snake (JavaRush)

--- src/snakejavarush/Room.java
+++ src/snakejavarush/Room.java
@@ -2,14 +2,18 @@
 
 package snakejavarush;
 
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Room {
 
     private int width, height;
     private Snake snake;
     private Mouse mouse;
 
-    static Room game;
-    
+    public static Room game;
+
     public int getWidth() {
         return width;
     }
@@ -46,19 +50,20 @@ public class Room {
         this.width = width;
         this.height = height;
         this.snake = snake;
+        
     }
     
     public void createMouse(){
         int x = (int)(Math.random()*width);
         int y = (int)(Math.random()*height);
-        Mouse mouse = new Mouse(x, y);
+        mouse = new Mouse(x, y);
     }
     
     public void eatMouse(){
         createMouse();
     }
     
-    public void sleep() throws InterruptedException{
+    public void sleepG(){
         int millisSleep = 500;
         int millisSleepMin = 200;
         int snakeSize = snake.sections.size();
@@ -68,24 +73,45 @@ public class Room {
             millisSleep = millisSleep - (millisSleep - millisSleepMin)*snakeSize/15;
         }
         
-        Thread.sleep(millisSleep);
+        try {
+            Thread.sleep(millisSleep);
+        } catch (InterruptedException ex) {
+            System.out.println("InterruptedException: " + ex.getMessage());
+        }
     }
     
     public void run(){
-        int[][] screen = new int[20][20];
+        game.print();
+        //game.sleepG();
     }
     
     public void print(){
+        int[][] screen = new int[width][height];
+        screen[snake.getX()][snake.getY()] = 2; // голова змеи, [0,0]
+
+        if (snake.sections.size() > 1){
+            for(int i=1; i<snake.sections.size(); i++){
+                screen[snake.sections.get(i).getX()][snake.sections.get(i).getY()] = 1; // тело змеи
+            }
+        }
+
+        screen[game.mouse.getX()][game.mouse.getY()] = 3; // мышь
         
+        String [] symb = {".", "x", "X", "+"};
+        for(int i=0; i<width; i++){
+            for(int j = 0; j<height; j++){
+                System.out.print(symb[screen[i][j]]);
+            }
+            System.out.println();
+        }
     }
     
     public static void main(String[] args) {
         Snake snake = new Snake(10, 10);
-        Room room = new Room(20, 20, snake);
-        snake.direction = SnakeDirection.DOWN;
-        
-        room.createMouse();
-        room.run();
+        game = new Room(20, 20, snake);
+        game.snake.setDirection(SnakeDirection.DOWN);
+        game.createMouse();
+        game.run();
     }
     
 }
